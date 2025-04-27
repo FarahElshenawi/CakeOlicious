439ea90aa753613950eaf4d2efa1309e8d2b7879
FarahElshenawi <farah.elshenawy2@gmail.com>
2025-04-27 16:42:55 +0300

    Modify models to be like our database

--- backend/config/config.py
+++ backend/config/config.py
@@ -1,1 +1,9 @@
- 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+class Config:
+    '''Base configuration class'''
+    SECRET_KEY= os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URL= os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
